4bbcd082638bac9d56882908050a77c844496b48
Feras <feras.ahmad@gmx.de>
2023-10-27 22:17:09 +0200

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>        
+      <p className="read-the-docs">
+        Personal Portfolio
+      </p>
+
+      <p className="read-the-docs">
+        Cooming soon ....
+      </p>
+    </div>
+  )
+}
+
+export default App
