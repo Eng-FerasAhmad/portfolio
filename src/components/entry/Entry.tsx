b8b083f07e11c50082fee2d00e2b19402f342081
Feras <feras.ahmad@gmx.de>
2023-10-27 22:53:03 +0200

    test new branch

--- src/components/entry/Entry.tsx
+++ src/components/entry/Entry.tsx
@@ -2,7 +2,7 @@ export default function Entry(): JSX.Element {
   return (
     <div className="entry" data-testid="entry">
       <div className='entry-form'>
-        Personal Portfolio Commin soon
+        Personal Portfolio Comming soon...
       </div>
     </div>
   );
