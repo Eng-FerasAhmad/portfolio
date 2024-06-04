87fa8b943cbe4649538b2450d94922a2d7275031
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-06-04 22:50:19 +0200

    remove border from scroll button

--- src/library/scroll-button/styles.ts
+++ src/library/scroll-button/styles.ts
@@ -18,11 +18,8 @@ export const ButtonContainer = styled.div`
     }
 
     border: 0 solid;
-    box-shadow: inset 0 0 20px rgb(255 255 255 / 0%);
-    outline: 1px solid rgb(255 255 255 / 50%);
     outline-offset: 0;
     text-shadow: none;
-    transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
 
     &:hover {
         cursor: pointer;
